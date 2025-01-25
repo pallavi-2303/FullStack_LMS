bac2cb9658980a4402e442cdb9a328043e2356dc
pallavi-2303 <singh.pallavikumari14@gmail.com>
2025-01-26 01:30:10 +0530

    changes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserProfile, login, logout, register, updateProfile } from "../Controllers/user.controller.js";
+import { getUserProfile, login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
 const router = express.Router();
